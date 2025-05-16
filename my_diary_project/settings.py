5f6a7efb810fad883ec764abf851b7dd35c8ebed
amir dwikat <amirdwikat@gmail.com>
2025-05-16 11:33:07 +0200

    switching databases

--- my_diary_project/settings.py
+++ my_diary_project/settings.py
@@ -78,17 +78,43 @@ WSGI_APPLICATION = 'my_diary_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'HOST' : 'u615qyjzybll9lrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-        'USERNAME' : 'secbydoh7yxgfppj',
-        'PASSWORD' : 'oux0ol0y50wpbq3q',
-        'PORT' : '3306',
-        'DATABASE' : 'v1y9spi92oaqpgb0',
+        'HOST' : 'u615qyjzybll9lrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',#os.environ['DB_HOST'],
+        'USERNAME' : 'yqub2iquk871u0sq',#os.environ['DB_USERNAME'],
+        'PASSWORD' : 'doc27gqllmfqmfhc',#os.environ['DB_PASSWORD'],
+        'PORT': '3306',
+        'DATABASE' : 'k11pi2l13e0y96tu',
     }
 }
+
+
+
+
+
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         #'NAME': BASE_DIR / 'db.sqlite3',
+#         'HOST' : 'u615qyjzybll9lrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+#         'USERNAME' : 'secbydoh7yxgfppj',
+#         'PASSWORD' : 'oux0ol0y50wpbq3q',
+#         'PORT' : '3306',
+#         'DATABASE' : 'v1y9spi92oaqpgb0',
+#     }
+# }
 
 
 # Password validation
